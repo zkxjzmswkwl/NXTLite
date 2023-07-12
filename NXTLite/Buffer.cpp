250fc1981dcf43e351915a19f22e55b4c64bc674
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2023-07-11 22:51:53 -0600

    Update Buffer.cpp

--- NXTLite/Buffer.cpp
+++ NXTLite/Buffer.cpp
@@ -1,41 +1,60 @@
 #include "pch.h"
-#include "Buffer.h"
+#include "packet.h"
 #include <array>
+#include <stdint.h>
+#include <iomanip>
 
-Buffer::Buffer()
+Packet::Packet()
 {
 	m_offset = 0;
+	memset(this->m_array, 0, 512);
 }
 
-
-void Buffer::writeByte(int toWrite)
+void Packet::writeByte(int32_t toWrite)
 {
 	m_array[++m_offset - 1] = (BYTE)toWrite;
 }
 
-void Buffer::writeShort(int toWrite)
+void Packet::writeShort(int32_t toWrite)
 {
 	m_array[++m_offset - 1] = (BYTE)(toWrite >> 8);
-	writeByte(toWrite);
+	m_array[++m_offset - 1] = (BYTE)toWrite;
 }
 
-void Buffer::writeInt(int toWrite)
+void Packet::writeInt(int toWrite)
 {
 	m_array[++m_offset - 1] = (BYTE)(toWrite >> 24);
 	m_array[++m_offset - 1] = (BYTE)(toWrite >> 16);
 	m_array[++m_offset - 1] = (BYTE)(toWrite >> 8);
-	writeByte(toWrite);
+	m_array[++m_offset - 1] = (BYTE)(toWrite);
 }
 
-void Buffer::writeBool(bool toWrite)
+void Packet::writeBool(bool toWrite)
 {
 	writeByte(toWrite ? 1 : 0);
+	m_offset++;
 }
 
-void Buffer::print()
+int Packet::readShort()
 {
-    for (size_t i = 0; i < std::end(m_array)-std::begin(m_array); ++i) {
-        printf("%01x ", (uint8_t)m_array[i]);
-    }
-	printf("\n");
+	m_offset += 2;
+	return ((m_array[m_offset - 2] & 0xff) << 8) + (m_array[m_offset - 1] & 0xff);
+}
+
+int Packet::readInt()
+{
+	m_offset += 4;
+	return 	((m_array[m_offset -4]  & 0xff) << 24)  +
+			((m_array[m_offset - 3] & 0xff) << 16)  +
+			((m_array[m_offset - 2] & 0xff) << 8)   + 
+			(m_array[m_offset - 1]  & 0xff);
+}
+
+void Packet::print()
+{
+	for (int i = 0; i < m_offset; ++i)
+	{
+		std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)(m_array[i]) << " ";
+	}
+	std::cout << std::dec << "\n";
 }
